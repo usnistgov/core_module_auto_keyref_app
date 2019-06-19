f547a8e79538da5a2dd1e8e09a7aa26adb530631
Guillaume Sousa Amaral <guillaume.sousa@nist.gov>
2019-06-19 17:04:41 -0400

    chore: optimize imports
    Change-Id: I850e8de57591e725422ae9b2dd9eced19b19476a

--- core_module_auto_keyref_app/urls.py
+++ core_module_auto_keyref_app/urls.py
@@ -2,8 +2,8 @@
 """
 from django.conf.urls import url
 
-from core_module_auto_keyref_app.views import AutoKeyRefModule
 from core_module_auto_keyref_app.ajax import get_updated_keys
+from core_module_auto_keyref_app.views import AutoKeyRefModule
 
 urlpatterns = [
     url(r'module-auto-keyref', AutoKeyRefModule.as_view(), name='core_auto_keyref_view'),
