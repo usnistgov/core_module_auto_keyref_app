ee7d5ac0c20e9dd9d2cfc6bd2110bf78693ebec5
Guillaume Sousa Amaral <guillaume.sousa@nist.gov>
2019-09-27 15:38:28 -0400

    feat: upgrade to Django 2.2
    Change-Id: I0a811d76300ead9a4f81c1b6b2ceb8a52d7bda92

--- core_module_auto_keyref_app/urls.py
+++ core_module_auto_keyref_app/urls.py
@@ -1,11 +1,12 @@
 """Url routing
 """
-from django.conf.urls import url
+
+from django.urls import re_path
 
 from core_module_auto_keyref_app.ajax import get_updated_keys
 from core_module_auto_keyref_app.views import AutoKeyRefModule
 
 urlpatterns = [
-    url(r'module-auto-keyref', AutoKeyRefModule.as_view(), name='core_auto_keyref_view'),
-    url(r'get-updated-keys', get_updated_keys, name='core_module_auto_keyref_app_get_updated_keys')
+    re_path(r'module-auto-keyref', AutoKeyRefModule.as_view(), name='core_auto_keyref_view'),
+    re_path(r'get-updated-keys', get_updated_keys, name='core_module_auto_keyref_app_get_updated_keys')
 ]
