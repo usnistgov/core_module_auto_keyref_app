ab2714ff2b35af094483f571eabfea572856ed4b
Guillaume Sousa Amaral <guillaume.sousa@nist.gov>
2019-10-24 14:50:26 -0400

    fix: revert django upgrade
    Change-Id: Ia85b0739d2743605d1c921489c3ac85f54387937

--- core_module_auto_keyref_app/urls.py
+++ core_module_auto_keyref_app/urls.py
@@ -1,12 +1,11 @@
 """Url routing
 """
-
-from django.urls import re_path
+from django.conf.urls import url
 
 from core_module_auto_keyref_app.ajax import get_updated_keys
 from core_module_auto_keyref_app.views import AutoKeyRefModule
 
 urlpatterns = [
-    re_path(r'module-auto-keyref', AutoKeyRefModule.as_view(), name='core_auto_keyref_view'),
-    re_path(r'get-updated-keys', get_updated_keys, name='core_module_auto_keyref_app_get_updated_keys')
+    url(r'module-auto-keyref', AutoKeyRefModule.as_view(), name='core_auto_keyref_view'),
+    url(r'get-updated-keys', get_updated_keys, name='core_module_auto_keyref_app_get_updated_keys')
 ]
